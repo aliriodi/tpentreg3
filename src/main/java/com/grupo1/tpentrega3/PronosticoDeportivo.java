01f8fcf7ea178c09f22ec80895d3a30e6138e136
aliriodi <aliriodi@gmail.com>
2023-04-20 21:42:17 -0300

    Corre el TP.java y el PronosticoGrafico.java tambien...

--- src/main/java/com/grupo1/tpentrega3/PronosticoDeportivo.java
+++ src/main/java/com/grupo1/tpentrega3/PronosticoDeportivo.java
@@ -3,87 +3,84 @@ Para entrega 3
  */
 package com.grupo1.tpentrega3;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
-
-
 
 /**
  *
  * @author aguzman
  */
 public class PronosticoDeportivo {
+
     private ListaEquipos equipos;
     private ListaPartidos partidos;
     private ListaParticipantes participantes;
-    private ListaPronosticos pronosticos; 
-   // private ListaParticipantes participantesOrdenados;
-   // private int i;
+    private ListaPronosticos pronosticos;
+   
+    // private ListaParticipantes participantesOrdenados;
+    // private int i;
 
     public PronosticoDeportivo() {
         equipos = new ListaEquipos();
         partidos = new ListaPartidos();
         participantes = new ListaParticipantes();
-        pronosticos = new ListaPronosticos(); 
-     //   participantesOrdenados = new ListaParticipantes();
-     //   i=0;
+        pronosticos = new ListaPronosticos();
+        //   participantesOrdenados = new ListaParticipantes();
+        //   i=0;
     }
 
-    public void play(){
+    public void play() {
+       
+
         // cargar y listar los equipos
         equipos.cargarDeBD();
         //equipos.cargarDeArchivo();
         System.out.println("Los equipos cargados son: " + equipos.listar());
- 
-
-      //  partidos.cargarDeArchivo(equipos);
+       
+        //  partidos.cargarDeArchivo(equipos);
         partidos.cargarDeBD(equipos);
         System.out.println("Los partidos cargados son: " + partidos.listar());
-      
-      //  pronosticos.cargarDeArchivo();
-        
-      //  participantes.cargarDeArchivo();
+         
+        //  pronosticos.cargarDeArchivo();
+        //  participantes.cargarDeArchivo();
         participantes.cargarDeBD();
         // Una vez cargados los participantes, para cada uno de ellos
         // cargar sus pronósticos
-    
-       // for (Participante p : participantes.getParticipantes()) {
-       //      System.out.println("Cargando pronosticos de Archivo");
-          //     p.cargarPronosticos(pronosticos);
-       // }
+
+        // for (Participante p : participantes.getParticipantes()) {
+        //      System.out.println("Cargando pronosticos de Archivo");
+        //     p.cargarPronosticos(pronosticos);
+        // }
         System.out.println("Cargando pronosticos de BD por cada participante");
-         for (Participante p : participantes.getParticipantes()) {
-               System.out.println("Cargando pronosticos para"+p);
+        for (Participante p : participantes.getParticipantes()) {
+            System.out.println("Cargando pronosticos para" + p);
             p.cargarPronosticosBD(equipos, partidos);
         }
-        
-        System.out.println("\n"+"Los participantes cargados son: " + participantes.listar());
-         System.out.println( "\n"+"Los participantes ordenados por ID y sus puntajes son:");   
-        System.out.println( "Nombre                 " + "Puntos");
-       for(Participante participante : participantes.getParticipantes()) {
+
+        System.out.println("\n" + "Los participantes cargados son: " + participantes.listar());
+        System.out.println("\n" + "Los participantes ordenados por ID y sus puntajes son:");
+        System.out.println("Nombre                 " + "Puntos");
+        for (Participante participante : participantes.getParticipantes()) {
             switch (participante.getNombre().length()) {
                 case 17:
-                    System.out.println(  participante.getNombre()+"         "+participante.resultados());
+                    System.out.println(participante.getNombre() + "         " + participante.resultados());
+                   
                     break;
                 case 16:
-                    System.out.println(  participante.getNombre()+"          "+participante.resultados());
+                    System.out.println(participante.getNombre() + "          " + participante.resultados());
                     break;
                 case 15:
-                    System.out.println(  participante.getNombre()+"           "+participante.resultados());
+                    System.out.println(participante.getNombre() + "           " + participante.resultados());
                     break;
                 case 14:
-                    System.out.println(  participante.getNombre()+"            "+participante.resultados());
+                    System.out.println(participante.getNombre() + "            " + participante.resultados());
                     break;
-                 case 13:
-                    System.out.println(  participante.getNombre()+"             "+participante.resultados());
-                    break;    
+                case 13:
+                    System.out.println(participante.getNombre() + "             " + participante.resultados());
+                    break;
                 default:
-                    System.out.println(  participante.getNombre()+participante.getNombre().length()+"       "+participante.resultados());
+                    System.out.println(participante.getNombre() + participante.getNombre().length() + "       " + participante.resultados());
                     break;
             }
-    }
+        }
 
         //Ordenando Partcipantes
         /*
@@ -105,68 +102,75 @@ public class PronosticoDeportivo {
         participantesOrdenados.getParticipantes().add(P0);
         }
          */
-     
-        
-      //Reimprimiendo
-        System.out.println("\n"+"Los participantes Ordenados por puntos son: ");
+        //Reimprimiendo
+        System.out.println("\n" + "Los participantes Ordenados por puntos son: ");
 //           System.out.println( "Nombre               " + "Puntos");
 /*     for(Participante participante2 : participantesOrdenados.getParticipantes()) {
         
         System.out.println(  participante2.getNombre()+"         "+participante2.resultados());
         
     }
-*/  
- //System.out.println("Los participantes cargados son: " + participantes.listar());
-      System.out.println( "Nombre                 " + "Puntos");
-       for(Participante participante : participantes.getOrdenadosPorPuntaje()) {
-        
-         switch (participante.getNombre().length()) {
+         */
+        //System.out.println("Los participantes cargados son: " + participantes.listar());
+        System.out.println("Nombre                 " + "Puntos");
+        for (Participante participante : participantes.getOrdenadosPorPuntaje()) {
+
+            switch (participante.getNombre().length()) {
                 case 17:
-                    System.out.println(  participante.getNombre()+"         "+participante.resultados());
+                    System.out.println(participante.getNombre() + "         " + participante.resultados());
                     break;
                 case 16:
-                    System.out.println(  participante.getNombre()+"          "+participante.resultados());
+                    System.out.println(participante.getNombre() + "          " + participante.resultados());
                     break;
                 case 15:
-                    System.out.println(  participante.getNombre()+"           "+participante.resultados());
+                    System.out.println(participante.getNombre() + "           " + participante.resultados());
                     break;
                 case 14:
-                    System.out.println(  participante.getNombre()+"            "+participante.resultados());
+                    System.out.println(participante.getNombre() + "            " + participante.resultados());
                     break;
-                 case 13:
-                    System.out.println(  participante.getNombre()+"             "+participante.resultados());
-                    break;    
+                case 13:
+                    System.out.println(participante.getNombre() + "             " + participante.resultados());
+                    break;
                 default:
-                    System.out.println(  participante.getNombre()+participante.getNombre().length()+"       "+participante.resultados());
+                    System.out.println(participante.getNombre() + participante.getNombre().length() + "       " + participante.resultados());
                     break;
-         }
-    }     
-        System.out.println( "\n"+"El ganador o los ganadores son: ");
-        System.out.println( "Nombre                 " + "Puntos");
-        for(Participante p : participantes.getOrdenadosPorPuntaje()){
-            if(p.resultados()==participantes.getOrdenadosPorPuntaje().get(0).resultados())
-            {
-                            
-                 switch (p.getNombre().length()) {
-                case 17:
-                    System.out.println(  p.getNombre()+"         "+p.resultados());
-                    break;
-                case 16:
-                    System.out.println(  p.getNombre()+"          "+p.resultados());
-                    break;
-                case 15:
-                    System.out.println(  p.getNombre()+"           "+p.resultados());
-                    break;
-                case 14:
-                    System.out.println(  p.getNombre()+"            "+p.resultados());
-                    break;
-                 case 13:
-                    System.out.println(  p.getNombre()+"             "+p.resultados());
-                    break;    
-                default:
-                    System.out.println(  p.getNombre()+p.getNombre().length()+"       "+p.resultados());
-                    break;
-         }  }  }   
-    
-    } 
+            }
+        }
+        System.out.println("\n" + "El ganador o los ganadores son: ");
+        System.out.println("Nombre                 " + "Puntos");
+        for (Participante p : participantes.getOrdenadosPorPuntaje()) {
+            if (p.resultados() == participantes.getOrdenadosPorPuntaje().get(0).resultados()) {
+
+                switch (p.getNombre().length()) {
+                    case 17:
+                        System.out.println(p.getNombre() + "         " + p.resultados());
+                      
+                        break;
+                    case 16:
+                        System.out.println(p.getNombre() + "          " + p.resultados());
+                     
+                        break;
+                    case 15:
+                        System.out.println(p.getNombre() + "           " + p.resultados());
+                     
+                        break;
+                    case 14:
+                        System.out.println(p.getNombre() + "            " + p.resultados());
+                      
+                        break;
+                    case 13:
+                        System.out.println(p.getNombre() + "             " + p.resultados());
+                    
+                        break;
+                    default:
+                        System.out.println(p.getNombre() + p.getNombre().length() + "       " + p.resultados());
+                    
+                        break;
+                }
+            }
+        }
+
+    }
+
+   
 }
